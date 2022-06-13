27538b434b1c894ca130bf34a6d7f473d631c445
anilmar-thiago <anilmar-thiago@gmail.com>
2022-06-13 10:25:04 -0300

    Criação do Projeto

---
+++ src/Gerente.java
@@ -0,0 +1,19 @@
+
+public class Gerente extends Funcionario {
+
+	private double gratificacao;
+
+	public double getGratificacao() {
+		return gratificacao;
+	}
+
+	public void setGratificacao(double gratificacao) {
+		this.gratificacao = gratificacao;
+	}
+	
+	public abstract void listarFuncionario()
+	{
+		
+	}
+
+}
